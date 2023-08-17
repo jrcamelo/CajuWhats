e8ee68a1e0399d45cbfe8f1ba516286b6838cfd2
jrcamelo <joaorafaelx@gmail.com>
2023-08-17 22:42:56 +0000

    Add retry on 429

--- lib/twilio_client.ex
+++ lib/twilio_client.ex
@@ -1,11 +1,8 @@
 defmodule CajuWhats.TwilioClient do
   use HTTPoison.Base
+  require Logger
 
   @twilio_url "https://api.twilio.com/2010-04-01/Accounts/AC23bf52cbca125a4c92d7268e3f28d82f/Messages.json"
-
-  defp key do
-    Application.fetch_env!(:caju_whats, :openai_key)
-  end
 
   defp auth_token do
     "Basic " <> Base.encode64("AC23bf52cbca125a4c92d7268e3f28d82f:" <> Application.fetch_env!(:caju_whats, :twilio_auth_token))
@@ -18,15 +15,31 @@ defmodule CajuWhats.TwilioClient do
     ]
   end
 
-  def send_message(to, from, body) do
+  def send_message(to, from, message_body, retry_count \\ 0) do
+    max_retries = 2
+
     payload = [
       {"To", to},
       {"From", from},
-      {"Body", body}
+      {"Body", message_body}
     ]
     payload_encoded = URI.encode_query(payload)
 
-    post(@twilio_url, payload_encoded, headers())
+    case post(@twilio_url, payload_encoded, headers()) do
+      {:ok, %HTTPoison.Response{status_code: 200}} ->
+        Logger.info("Message sent successfully to #{to}")
+        :ok
+      {:ok, %HTTPoison.Response{status_code: 429}} when retry_count < max_retries ->
+        Logger.warn("Got a 429 when talking to #{to}, waiting 3 seconds + #{retry_count * 3} seconds")
+        :timer.sleep(3000 + (3000 * retry_count))
+        send_message(to, from, message_body, retry_count + 1)
+      {:ok, %HTTPoison.Response{status_code: status_code, body: body}} ->
+        Logger.error("Failed to send message to #{to}: Status code #{status_code}, Body: #{body}")
+        {:error, status_code}
+      {:error, reason} ->
+        Logger.error("Failed to send message to #{to}: #{inspect(reason)}")
+        {:error, reason}
+    end
   end
 
   def download_from_url(url) do
